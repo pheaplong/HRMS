17b3cce4a2306364d9dee71ed8577038aeaa7ce1
shadowcambodia <stefencambodia@gmail.com>
2020-04-21 13:28:28 +0700

    21042020_1328

--- Test/PermissionLogic.test.js
+++ Test/PermissionLogic.test.js
@@ -1,18 +1,28 @@
-const PermissionLogic = require('../DomainProccessor/PermissionProccess')
+const PermissionLogic = require('../BussinessLogic/PermissionLogic')
 const permissionLogic = new PermissionLogic()
-test('should be more than 0', async () => {
-   let result = null
-   result = await permissionLogic.loadDataByID(47, 1)
-   expect(result.isSuccessed).toBe(true)
+describe('Permission', () => {
+   test('should be PermissionQualify true', async () => {
+      let result = null
+      result = await permissionLogic.isPermissionQualify(47, 1)
+      expect(result.isSuccessed).toBe(true)
+   })
+   test('should be PermissionQualify true', async () => {
+      let result = null
+      result = await permissionLogic.isPermissionQualify(47, 1)
+      expect(result.isSuccessed).toBe(true)
+   })
+   test('should be PermissionQualify true', async () => {
+      let result = null
+      result = await permissionLogic.isPermissionQualify(47, 1)
+      expect(result.isSuccessed).toBe(true)
+   }
+   )
+   test('Message should contain Cannot Connect to Database', async () => {
+      let result = null
+      result = await permissionLogic.isPermissionQualify(47, 1)
+      expect(result.message).toMatch(/to Database/)
+      console.log(result.message);
 
-   result = await permissionLogic.loadDataByID(47, 1)
-   expect(result.isSuccessed).toBe(true)
-   result = await permissionLogic.loadDataByID(47, 1)
-   expect(result.isSuccessed).toBe(true)
-   result = await permissionLogic.loadDataByID(47, 1)
-   expect(result.isSuccessed).toBe(true)
-   result = await permissionLogic.loadDataByID(47, 1)
-   expect(result.isSuccessed).toBe(true)
-
+   })
 }
 )
